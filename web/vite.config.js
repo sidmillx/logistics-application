e175dc482e306f866bad57c4bf4a88f622e7ee8c
sidmillx <mhlengisid@gmail.com>
2025-07-13 09:14:50 +0200

    revert

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/icons/*.svg',
-          dest: 'assets/icons'
-        }
-      ]
-    })
-  ],
-  build: {
-    assetsInclude: ['**/*.svg'], // Explicitly include SVGs
-  }
+  plugins: [react()],
 })
