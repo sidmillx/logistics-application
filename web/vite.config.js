df7b4ac6bacc4593bf5be12cdab9be1bb71685b1
sidmillx <mhlengisid@gmail.com>
2025-07-13 09:12:35 +0200

    fix/allow svgs in vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/icons/*.svg',
+          dest: 'assets/icons'
+        }
+      ]
+    })
+  ],
+  build: {
+    assetsInclude: ['**/*.svg'], // Explicitly include SVGs
+  }
 })
